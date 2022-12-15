ed5da5b6aab1c19451744d79714c2bc4610ef8a1
khyojun <nandong5923@naver.com>
2022-12-15 15:54:59 +0900

    refactor: audience에 대한 캡슐화를 진행

--- src/main/java/org/example/Audience.java
+++ src/main/java/org/example/Audience.java
@@ -8,7 +8,19 @@ public class Audience {
         this.bag = bag;
     }
 
-    public Bag getBag() {
-        return bag;
+    public long buy(Ticket ticket){
+        if(bag.hasInvitation()){
+            bag.setTicket(ticket);
+            return 0L;
+        }
+        else{
+           bag.minusAmount(ticket.getFee());
+           bag.setTicket(ticket);
+           return ticket.getFee();
+        }
+
     }
+
+
+
 }
